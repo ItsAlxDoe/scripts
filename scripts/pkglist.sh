c304d2664b581cdf5d25cbaea49ebad0cae87efb
Alicia <AlxDoe@outlook.com>
2022-03-20 04:07:04 -0400

    Cleanup and package additions
    I did this on my phone lol
    Might make it interactive via basic shell scripting later

--- scripts/pkglist.sh
+++ scripts/pkglist.sh
@@ -1,6 +1,22 @@
-#Intended for EndeavourOS. Installs packages from repos and AUR after fresh olline install for my convenience
+#Intended for EndeavourOS
+#Installs packages after offline install for my convenience
 
+#System
+#Remove yay and install paru
 sudo pacman -R yay && sudo pacman -S paru
-
 paru -S timeshift
+sudo pacman -S keepassxc
+sudo pacman -S gnome-disk-utility
+
+#Media
+sudo pacman -S blueman
 sudo pacman -S discord
+
+
+
+#Remove parole and install SMPlayer
+sudo pacman -R parole && sudo pacman -S smplayer
+
+#Misc
+sudo pacman -S neofetch
+sudo pacman -S pfetch
