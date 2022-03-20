90bfae7efa6046ef5504e8671698b5d5599e6030
Alicia <AlxDoe@outlook.com>
2022-03-20 16:32:11 -0400

    More cleanup before move

--- scripts/pkglist.sh
+++ scripts/pkglist.sh
@@ -1,21 +1,33 @@
-#Intended for EndeavourOS
+#!/bin/sh
+#Intended for EndeavourOS 21.5 Atlantis Neo
 #Installs packages after offline install for my convenience
+ 
+#Script name        : Install personal list of packages
+#Author             : Alicia Díaz
+#
+#
+#
+#
+#
+
 
 #System
 #Remove yay and install paru
 sudo pacman -R yay && sudo pacman -S paru
+
 paru -S timeshift
 sudo pacman -S keepassxc
 sudo pacman -S gnome-disk-utility
 
 #Media
+#Remove parole and install SMPlayer
+sudo pacman -R parole && sudo pacman -S smplayer
 sudo pacman -S blueman
 sudo pacman -S discord
 
+#Gaming
+sudo pacman -S steam
 
-
-#Remove parole and install SMPlayer
-sudo pacman -R parole && sudo pacman -S smplayer
 
 #Misc
 sudo pacman -S neofetch
